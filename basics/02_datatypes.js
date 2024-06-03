82b2e741bd49b2675c4075bad29a24f9714b0724
Vishal <97728453+2k21a1316@users.noreply.github.com>
2024-06-03 14:42:15 +0000

    datatypes

---
+++ basics/02_datatypes.js
@@ -0,0 +1,32 @@
+//tc39,mdn javascript documentation
+// use string in single or double quate 
+
+"use strict"; // treat all JS code as newer version
+
+// alert( 3 + 3) // we are using nodejs, not browser
+
+console.log(3 
+    +
+     3) // code readability should be high //this coede is not readable
+
+console.log("Hitesh") //this code has high readability
+
+
+let name = "hitesh"
+let age = 18
+let isLoggedIn = false
+let state;
+
+// primitive datatypes
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => 
+// symbol => unique
+
+
+// object
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object //language error 
