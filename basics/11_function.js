2579e6f16292a968225d9e6bc22d380b9077e88e
Vishal <97728453+2k21a1316@users.noreply.github.com>
2024-06-04 16:20:03 +0000

    function

---
+++ basics/11_function.js
@@ -0,0 +1,76 @@
+
+function sayMyName(){
+    console.log("H");
+    console.log("I");
+    console.log("T");
+    console.log("E");
+    console.log("S");
+    console.log("H");
+}
+// sayMyName reference of function 
+// sayMyName() execution of function
+
+// function addTwoNumbers(number1, number2){
+
+//     console.log(number1 + number2);//number type is must for number operation
+// }
+
+// not code execute after return 
+//console does not return only print
+// to return we use return 
+function addTwoNumbers(number1, number2){
+
+    // let result = number1 + number2
+    // return result
+    return number1 + number2
+}
+
+const result = addTwoNumbers(3, 5)
+
+// console.log("Result: ", result);
+
+
+function loginUserMessage(username = "sam"){
+    if(!username){
+        // use ===to check condition
+        console.log("PLease enter a username");
+        return
+    }
+    return `${username} just logged in`
+}
+
+// console.log(loginUserMessage("hitesh"))
+// console.log(loginUserMessage("hitesh"))
+
+
+function calculateCartPrice(val1, val2, ...num1){//... called rest and spread operator depends on its usecase 
+    //rest combined the value in array
+    // spread spread the values 
+    return num1//500 2000
+}
+
+// console.log(calculateCartPrice(200, 400, 500, 2000))
+
+const user = {
+    username: "hitesh",
+    prices: 199
+}
+
+function handleObject(anyobject){
+    console.log(`Username is ${anyobject.username} and price is ${anyobject.price}`);
+}
+
+// handleObject(user)
+handleObject({
+    username: "sam",
+    price: 399
+})
+
+const myNewArray = [200, 400, 100, 600]
+
+function returnSecondValue(getArray){
+    return getArray[1]
+}
+
+// console.log(returnSecondValue(myNewArray));
+console.log(returnSecondValue([200, 400, 500, 1000]));
