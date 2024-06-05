8ceefd591c6495cc36eb39808496df00c802ec24
Vishal <97728453+2k21a1316@users.noreply.github.com>
2024-06-05 15:27:56 +0000

    forin

---
+++ iteration/three.js
@@ -0,0 +1,26 @@
+const myObject = {
+    js: 'javascript',
+    cpp: 'C++',
+    rb: "ruby",
+    swift: "swift by apple"
+}
+
+for (const key in myObject) {
+    //console.log(`${key} shortcut is for ${myObject[key]}`);
+}
+
+const programming = ["js", "rb", "py", "java", "cpp"]
+
+for (const key in programming) {
+    //console.log(programming[key]);//in forin loop for array key gives key but in forof loop key gives values if we access direct key
+}
+
+// const map = new Map() for in map is not iteratable
+// map.set('IN', "India")
+// map.set('USA', "United States of America")
+// map.set('Fr', "France")
+// map.set('IN', "India")
+
+// for (const key in map) {
+//     console.log(key);
+// }
