37c88acdc84dd876d31cceb516561607b8b1ca0a
Vishal <97728453+2k21a1316@users.noreply.github.com>
2024-06-05 15:54:24 +0000

    foreach

---
+++ iteration/foreach.js
@@ -0,0 +1,43 @@
+const coding = ["js", "ruby", "java", "python", "cpp"]
+
+// with reguler function 
+// coding.forEach( function (val){//this function is running in array
+//     console.log(val);//no need to give increment and length condition and initialisation
+// } )
+
+// call back function not having name
+// with arrow function 
+// coding.forEach( (item) => {
+//     console.log(item);
+// } )
+
+// function printMe(item){
+//     console.log(item);
+// }
+
+// give reference of existing function not execution
+// coding.forEach(printMe)
+
+// coding.forEach( (item, index, arr)=> {
+//     console.log(item, index, arr);
+// } )
+
+const myCoding = [
+    {
+        languageName: "javascript",
+        languageFileName: "js"
+    },
+    {
+        languageName: "java",
+        languageFileName: "java"
+    },
+    {
+        languageName: "python",
+        languageFileName: "py"
+    },
+]
+
+myCoding.forEach( (item) => {
+    
+    console.log(item.languageName);
+} )
